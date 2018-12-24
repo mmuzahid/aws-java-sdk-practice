bbde9ad9a24c8f63ee7a42bc4501e2e474ce1ff8
mmuzahid <muzahid.engr@gmail.com>
2018-12-24 10:40:50 +0600

    Added Maven Initial Project

---
+++ src/main/java/me/practice/awsjavasdk/App.java
@@ -0,0 +1,14 @@
+package me.practice.awsjavasdk;
+
+import java.io.IOException;
+import java.security.spec.InvalidKeySpecException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws InvalidKeySpecException, IOException {
+		System.out.println("Hello World!");
+	}
+}
